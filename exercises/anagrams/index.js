4750ea4193e278dd2312e1ca78f556aa8d92a7f2
Veronika Dodda <v.shengeliya@gmail.com>
2020-11-24 17:36:39 -0500

    anagral solution1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,38 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+    //need to build a helper fucntion buildCharMap to clear up the strings and build an object
+
+    const aCharMap = buildCharMap(stringA)
+    const bCharMap = buildCharMap(stringB)
+
+    //then compare if the length of keys of both strings are the same, if yes then ..
+
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+        return false
+    } 
+   
+    for (let char of stringA){
+    //.. compare if the quantity of each key are the same exp. strA[h]=1, strB[h]=1, if 1===1, return true
+        if (aCharMap[char]!= bCharMap[char]){
+            return false
+        }
+    }
+    return true
+}
+
+    function buildCharMap(str){
+
+        const charMap= {}
+
+        for (let char of str.replace(/[^\w]/g, "").toLowerCase()){
+            charMap[char] = charMap[char]+1 || 1;
+        }
+        return charMap
+    }
+
+anagrams('hello', 'lleho!!!?><')
 
 module.exports = anagrams;
