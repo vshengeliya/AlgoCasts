3357eea0c3c525483d77e6f1cced3349046e6da7
Veronika Dodda <v.shengeliya@gmail.com>
2020-12-07 16:36:08 -0500

    pyramid solved

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,33 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+
+    // From 0 to n (iterate through rows)
+    //Create an empty string, 'level'
+    // From 0 to ??(columns)
+    //IF the column sould have a #
+    //Add a '#' to 'level'
+    //ELSE
+    //Add a space to 'level'
+    //Console log 'level'
+
+    const midpoint = Math.floor(( 2 * n - 1)/2);
+
+        for (let row = 0; row < n; row++ ){
+            let level = ''
+            
+                for (column = 0; column < 2 * n-1; column++){
+                    if ( midpoint - row <= column && midpoint + row >= column){
+                        level += '#'
+                    } else{
+                        level += ' '
+                    }
+                }
+            console.log(level)
+        }  
+}
+
+console.log(pyramid(3))
 
 module.exports = pyramid;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -1,45 +1,45 @@
-// // --- Directions
-// // Write a function that accepts a positive number N.
-// // The function should console log a step shape
-// // with N levels using the # character.  Make sure the
-// // step has spaces on the right hand side!
-// // --- Examples
-// //   steps(2)
-// //       '# '
-// //       '##'
-// //   steps(3)
-// //       '#  '
-// //       '## '
-// //       '###'
-// //   steps(4)
-// //       '#   '
-// //       '##  '
-// //       '### '
-// //       '####'
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
 
-// function steps(n) {
-//     // From 0 to n (iterate througt rows)
-//         //Create an empty string, 'stair'
-//         //From 0 to n (iterate througt columns)
-//             //If the current column is equal to or less that the current row
-//                 //Add a '#' to 'stair'
-//             //Else
-//                 //Add space to 'stair'
-//         //Console log 'stair'
+function steps(n) {
+    // From 0 to n (iterate througt rows)
+        //Create an empty string, 'stair'
+        //From 0 to n (iterate througt columns)
+            //If the current column is equal to or less that the current row
+                //Add a '#' to 'stair'
+            //Else
+                //Add space to 'stair'
+        //Console log 'stair'
 
-//    for (let row=0; row <n; row++){
-//        let stair = ''
+   for (let row=0; row <n; row++){
+       let stair = ''
        
-//        for (let column = 0; column < n; column++){
-//            if (column <= row){
-//                stair += '#'
-//            } else{
-//                stair += ' '
-//            }
-//        }
-//        console.log(stair)
-//    }
-// }
+       for (let column = 0; column < n; column++){
+           if (column <= row){
+               stair += '#'
+           } else{
+               stair += ' '
+           }
+       }
+       console.log(stair)
+   }
+}
 
 //2nd solution using recurrsion
 
