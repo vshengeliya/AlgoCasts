26398e8af7d088b33890847ae42067546176c65c
Veronika Dodda <v.shengeliya@gmail.com>
2021-01-26 12:12:27 -0500

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,8 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
-//test
+function memoize(fn) {
+    const cache={}; // creating a record of all of the previous calls of the slowFib fucntion and store it
+    return function(...args) { // returtning an anonimus function that is or fib function in the last line of code with arguments //that recived to out slowFin fucntion, example: fib(5)
+      if (cache[args]) {
+        return cache[args];
+      };
+      const result = fn.apply(this, args);
+      cache[args] = result;
+      return result;
+    };
+ };
+
+ function slowFib(n) {
+     if ( n < 2) {
+    return n;
+    }
+     return fib(n - 1) + fib(n - 2);
+ };
+
+ const fib = memoize(slowFib);
+
+// function fib(n) {
+
+//     const result = [0, 1];
+
+//     for (let i = 2; i <= n; i++){
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+
+//         result.push(a + b)
+//     }
+//     return result[result.length-1]
+
+// }
 
 
 module.exports = fib;
